201101ad94a8b722a4278ab94f1fe25b7c5a42d4
Chukwuemeka Nwagu <c_nwagu@yahoo.com>
2019-03-17 21:16:11 +0100

    install lxml

--- src/PBL/htmlcleaner.py
+++ src/PBL/htmlcleaner.py
@@ -1,13 +1,14 @@
 
-
 """This class contains the method that converts a html document to pbl
-to make it compatible with the Pablo application"""
+to optimize it for the Pablo application."""
+
+from lxml import etree
 
 class HTMLCleaner():
 
     def clean(html):
+        pbl = html
+        pbl = pbl.replace('<html>', '<pbl>')
+        pbl = pbl.replace('</html>', '</pbl>')
 
-
-
-        
-        return html
+        return pbl
